7c3b2300dbd771eb9f26eac823b5d8564bf35125
sleep <2357088569@qq.com>
2022-10-20 10:25:31 +0800

    。

---
+++ day1/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+ <h1>app根组件</h1>
+ <TodoList :list="todolist"></TodoList>
+ </div>
+</template>
+
+<script>
+	import TodoList from './components/todolist.vue'
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+export default{
+	data(){return
+	{
+		todolist:[
+			{ id:1,task:'1',done:false },
+			{ id:2,task:'2',done:false },
+			{ id:3,task:'3',done:true }
+		]
+	}},
+	components:{ TodoList }
+	
+}
+</script>
+
+
+<style lang="less" scoped>
+
+</style>
---
+++ day1/src/components/todolist.vue
@@ -0,0 +1,31 @@
+<template>
+	<div><ul class="list-group">
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+		<div class='custom-control custom-checkbox'>
+	<input type="checkbox" class='custom-control-input' :id="item.id"/> 
+	<label class="custom-control-label" :for="item.id">xx</label>
+	</div>
+	<span class="badge badge-success badge-pill" v-if="item.done">完成</span>
+	<span class="badge badge-warning badge-pill" v-else>未完成</span>
+
+	 </li>
+  </ul>
+  </div>
+</template>
+
+<script>
+	export default{
+ name:'Todolist' ,
+	props:{
+		list:{
+			type:Array,
+			required:true,
+			default: [],
+			
+		}
+	}
+	}
+</script>
+
+<style lang="less" scoped>
+</style>
